b1c59cc85fca552f99bdd94a6e8a76f9f876c2d1
Mnemotechnician <69920617+mnemotechnician@users.noreply.github.com>
2026-05-17 17:06:50 +0300

    Why is it hardcoded in the pool manager

--- Content.IntegrationTests/PoolManager.Cvars.cs
+++ Content.IntegrationTests/PoolManager.Cvars.cs
@@ -1,4 +1,5 @@
 #nullable enable
+using Content.Shared._Floof.CCVar;
 using Content.Shared.CCVar;
 
 namespace Content.IntegrationTests;
@@ -35,6 +36,6 @@ public static partial class PoolManager
         (CCVars.InteractionRateLimitCount.Name, "9999999"),
         (CCVars.InteractionRateLimitPeriod.Name, "0.1"),
         (CCVars.MovementMobPushing.Name, "false"),
-        (CCVars.LavalandEnabled.Name, "false"), // Lavaland Change
+        (FloofCCVars.StationPlanetSpawning.Name, "false")
     };
 }

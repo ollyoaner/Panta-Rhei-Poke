49d06c3239bfc9b7c782627f935be80f423b79a0
Cprn-Sprkl <sprkl7992@gmail.com>
2026-06-20 19:40:29 -0400

    Cultral Disconnect Trait (#690)
    * Foreign Origin Trait
    
    * Update foreigner.yml
    
    * Update foreigner.ftl
    
    * Name

---
+++ Content.Shared/_Floof/Traits/Effects/RemoveSpeciesLanguageEffect.cs
@@ -0,0 +1,24 @@
+﻿using Content.Shared._DV.Traits.Effects;
+using Content.Shared._Floof.Language;
+using Content.Shared._Floof.Language.Systems;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Content.Shared._Floof.Traits.Effects;
+/// <summary>
+///     Removes all species granted languages.
+/// </summary>
+public sealed partial class RemoveSpeciesLanguageEffect : BaseTraitEffect
+{
+    public override void Apply(TraitEffectContext ctx)
+    {
+        var languageSys = ctx.EntMan.System<SharedLanguageSystem>();
+        var user = ctx.Player;
+        var languages = languageSys.GetUnderstoodLanguages(user).ShallowClone();
+        foreach (var language in languages)
+        {
+            if (language.Id != "TauCetiBasic")
+                languageSys.RemoveLanguage(user, language);
+        }
+    }
+}
--- Resources/Locale/en-US/_Floof/traits/foreigner.ftl
+++ Resources/Locale/en-US/_Floof/traits/foreigner.ftl
@@ -7,3 +7,8 @@ trait-foreigner-name = Foreigner
 trait-foreigner-desc =
     For some reason, you do not speak this station's primary language.
     Instead, you have been issued a translator that only you can use.
+
+trait-foreign-origin-name = Cultural Disconnect
+trait-foreign-origin-desc =
+    For some reason, you do not speak the common languages of your species.
+    Removes all languages granted by your species.
